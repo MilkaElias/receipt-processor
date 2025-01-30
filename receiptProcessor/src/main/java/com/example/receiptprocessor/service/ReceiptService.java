4881a74d548f05f581ff244c9f6fdb73578b4868
Milka Elias Worku <milkaelias23@gmail.com>
2025-01-30 15:05:44 -0600

    Add receipt service implementation

--- receiptProcessor/src/main/java/com/example/receiptprocessor/service/ReceiptService.java
+++ receiptProcessor/src/main/java/com/example/receiptprocessor/service/ReceiptService.java
@@ -1,16 +1,25 @@
 package com.example.receiptprocessor.service;
 
 import com.example.receiptprocessor.model.Receipt;
+import com.example.receiptprocessor.util.ProcessReceipt;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @Service
 public class ReceiptService {
+    private final Map<String, Integer> points=new HashMap<>();
 
     public String processReceipt(Receipt receipt) {
-        return "";
+        int point= ProcessReceipt.processReceipt(receipt);
+        String id= UUID.randomUUID().toString();
+        points.put(id,point);
+        return id;
     }
 
     public Integer getPoints(String id) {
-        return 0;
+        return points.getOrDefault(id,null);
     }
 }
---
+++ receiptProcessor/src/main/java/com/example/receiptprocessor/util/ProcessReceipt.java
@@ -0,0 +1,9 @@
+package com.example.receiptprocessor.util;
+
+import com.example.receiptprocessor.model.Receipt;
+
+public class ProcessReceipt {
+    public static Integer processReceipt(Receipt receipt){
+        return 0;
+    }
+}
