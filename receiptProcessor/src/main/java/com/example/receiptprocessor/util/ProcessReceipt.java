2aab397735e8d8c9cf46879c461fc768582cda68
Milka Elias Worku <milkaelias23@gmail.com>
2025-02-05 12:57:15 -0600

    Add clean implementation of the utility class

--- receiptProcessor/src/main/java/com/example/receiptprocessor/util/ProcessReceipt.java
+++ receiptProcessor/src/main/java/com/example/receiptprocessor/util/ProcessReceipt.java
@@ -2,47 +2,63 @@ package com.example.receiptprocessor.util;
 
 import com.example.receiptprocessor.model.Item;
 import com.example.receiptprocessor.model.Receipt;
-
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalTime;
+import java.util.List;
 
 public class ProcessReceipt {
     public static Integer calculatePoints(Receipt receipt){
         int points=0;
+        points +=alphanumericRetailerName(receipt.getRetailer());
+        points +=roundDollarTotal(receipt.getTotal());
+        points +=multipleOfQuarter(receipt.getTotal());
+        points +=everyTwoItems(receipt.getItems());
+        points +=oddPurchaseDate(receipt.getPurchaseDate());
+        points +=timeBetween2PMAnd4PM(receipt.getPurchaseTime());
+        points +=pointsBasedOnItemDescription(receipt.getItems());
 
-        //1 point for each alphanumeric character in the retailer name
-        points += receipt.getRetailer().replaceAll("[^a-zA-Z0-9]", "").length();
-
-        //50 points if the total os a round dollar amount with no cents
-        BigDecimal total= new BigDecimal(receipt.getTotal());
-        if(total.stripTrailingZeros().scale() <=0) points += 50;
-
-        //25 points if the total is a multiple of 0.25
-        if(total.remainder(new BigDecimal("0.25")).compareTo(BigDecimal.ZERO)==0) points+=25;
-
-        //5 points for every two items on the receipt
-        points += (receipt.getItems().size()/2) * 5;
-
-        //6 points if the date of purchase is odd
-        int day=Integer.parseInt(receipt.getPurchaseDate().split("-")[2]);
-        if(day % 2 !=0) points+=6;
-
-        //10 points if the time of purchase is between 2PM and 4PM
-        LocalTime time= LocalTime.parse(receipt.getPurchaseTime());
-        if(time.isAfter(LocalTime.of(14,0)) && time.isBefore(LocalTime.of(16,0))) points += 10;
+        return points;
+    }
 
 
-        //points based on item description
-        for(Item item: receipt.getItems()){
+    private static int alphanumericRetailerName(String retailer){
+        return retailer.replaceAll("[^a-zA-Z0-9]", "").length();
+    }
+
+    private static int roundDollarTotal(String total){
+        BigDecimal totalValue= new BigDecimal(total);
+        return (totalValue.stripTrailingZeros().scale() <=0) ? 50 : 0;
+    }
+
+    private static int multipleOfQuarter(String total){
+        BigDecimal totalValue= new BigDecimal(total);
+        return (totalValue.remainder(new BigDecimal("0.25")).compareTo(BigDecimal.ZERO)==0) ? 25:0;
+    }
+
+    private static int everyTwoItems(List<Item> items){
+        return items.size()/2 * 5;
+    }
+
+    private static int oddPurchaseDate(String purchaseDate){
+        int day=Integer.parseInt(purchaseDate.split("-")[2]);
+        return (day % 2 !=0) ? 6 :0;
+    }
+
+    private static int timeBetween2PMAnd4PM(String purchaseTime){
+        LocalTime time= LocalTime.parse(purchaseTime);
+        return time.isAfter(LocalTime.of(14,0)) && time.isBefore(LocalTime.of(16,0)) ? 10 : 0;
+    }
+
+    private static int pointsBasedOnItemDescription(List<Item> items){
+        int point=0;
+        for(Item item:items){
             String description= item.getShortDescription().trim();
             if(description.length() %3 ==0){
                 BigDecimal itemPrice= new BigDecimal(item.getPrice());
-                points += itemPrice.multiply(new BigDecimal(0.2)).setScale(0,RoundingMode.UP).intValue();
+                point += itemPrice.multiply(new BigDecimal(0.2)).setScale(0,RoundingMode.UP).intValue();
             }
         }
-
-
-        return points;
+        return point;
     }
 }
